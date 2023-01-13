7f0b3cf4d9762260cf70abd0c2c004d690ff864c
omkar lolge <omkarlolge119.ol@gmail.com>
2023-01-13 13:35:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Hero1 from "./Hero1";
+import { Routes,Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+import { createContext } from "react";
+import { useState } from "react";
+import Dash from "./Dashboard";
+import { useEffect } from "react";
+import axios from "axios";
+
+
+export const serverUrl = 'http://localhost:4000'
+export const MainContext = createContext();
+
 
 function App() {
+
+  const navigate = useNavigate();
+
+  const [admin,setAdmin] = useState(window.localStorage.getItem('admin'));
+
+  const [login,setLogin] = useState(window.localStorage.getItem('login'));
+
+  const [data,setData] = useState([])
+
+  useEffect(()=>{
+    (async ()=>{
+      await axios.get(`${serverUrl}/alldata`).then(response=>setData(response.data))
+    })()
+  },[data])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContext.Provider value={{admin, setAdmin, login, setLogin, data, setData}}>
+      <div className="App h-screen w-screen relative">
+       <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200">
+          <div className="max-w-5xl mx-auto px-4">
+            <div className="flex items-center justify-between h-16">
+              <span onClick={()=>{navigate("/")}} className="text-2xl text-gray-900 font-semibold">Logo</span>
+              <div className="flex space-x-4 text-gray-900 font-bold">
+               {login && <button onClick={()=>{navigate("/dashboard")}}>Dashboard</button>}
+               {login && <button onClick={()=>{setLogin(false);navigate("/signin")}}>Logout</button>}
+                {!login && <button onClick={()=>{navigate("/signin")}}>Sign In</button>}
+                {!login && <button onClick={()=>{navigate("/signup")}}>Sign Up</button>}
+              </div>
+            </div>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Hero1 />}/>
+          <Route path="/signin" element={<SignIn />}/>
+          <Route path="/signup" element={<SignUp />}/>
+          { login && <Route path="/dashboard" element={<Dash />}/>}
+        </Routes>
+      </div>
+    </MainContext.Provider>
   );
 }
 
---
+++ src/SignIn.js
@@ -0,0 +1,182 @@
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { MainContext } from "./App"; 
+import Alert from "@mui/material/Alert";
+
+import { serverUrl } from "./App";
+import axios from "axios";
+import { useContext } from "react";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
+export default function SignIn() {
+  const navigate = useNavigate();
+  const [response, setResponse] = useState("");
+  const [token, setToken] = useState("");
+  const [name, setName] = useState("");
+
+  const { admin, setAdmin, login, setLogin } = useContext(MainContext);
+
+  console.log(token, login, admin, name);
+ 
+  const loggedin = () =>
+  toast.success("Logged In!", {
+    position: "bottom-left",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover:false,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+
+
+  return (
+    <div className="flex h-auto items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
+        <div>
+          <h1 className="text-slate-500">Admin Account: username- tharun@gmail.com ; password- passwordtharun</h1>
+          <h1 className="text-slate-500">User Account: username- varun@gmail.com ; password- passwordvarun</h1>
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Sign In to your account
+          </h2>
+        </div>
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          validationSchema={Yup.object({
+            email: Yup.string()
+              .email("Invalid email address")
+              .required("Required"),
+            password: Yup.string()
+              .required("Password is required")
+              .min(5, "Your password is too short.")
+              .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
+          })}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            const { email, password } = values;
+            setResponse(null);
+            setTimeout(() => {
+              //   alert(JSON.stringify({email:email,password:password}, null, 2));
+              setSubmitting(false);
+            }, 400);
+            (async () => {
+              const url = `${serverUrl}/signin`;
+              const siginaxios = await axios
+                .post(url, {
+                  email,
+                  password,
+                })
+                .then((response) => {
+                  setResponse(response.data.msg);
+                  setToken(response.data.token);
+                  setName(response.data.name);
+                  setAdmin(response.data.admin);
+                  resetForm();
+                  if (response.data.msg === "logged in") {
+                    loggedin();
+                    window.localStorage.setItem("token", response.data.token);
+                    window.localStorage.setItem("user", response.data.name);
+                    window.localStorage.setItem("admin", response.data.admin);
+                    window.localStorage.setItem("login", true);
+                    setAdmin(response.data.admin.toString());
+                    setLogin(true);
+                    navigate("/dashboard");
+                  }
+                })
+                .catch((error) => alert(error, "error block activated"));
+              console.log(siginaxios);
+            })();
+          }}
+        >
+          <Form method="POST" className="mt-8 space-y-6">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <Field
+                  placeholder="Email"
+                  className="relative block w-full rounded-t-md appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  name="email"
+                  type="email"
+                />
+                <ErrorMessage sx={{ color: "red" }} name="email">
+                  {(msg) => <div style={{ color: "red" }}>{msg}</div>}
+                </ErrorMessage>
+              </div>
+              <div>
+                <Field
+                  placeholder="Password"
+                  className="relative block w-full rounded-b-md appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  name="password"
+                  type="password"
+                />
+                <ErrorMessage sx={{ color: "red" }} name="password">
+                  {(msg) => <div style={{ color: "red" }}>{msg}</div>}
+                </ErrorMessage>
+              </div>
+            </div>
+            {response ? (
+              <div>
+                {response === "logged in" ? (
+                  <Alert severity="success">{response}</Alert>
+                ) : (
+                  <div>
+                    <Alert severity="error">{response}</Alert>
+                    <a href="/">👉Create a new account now!!!👈</a>
+                  </div>
+                )}
+              </div>
+            ) : null}
+            <div>
+              {response === null ? (
+                <div class="flex justify-center">
+                  <div class="border-t-transparent border-solid animate-spin  rounded-full border-blue-400 border-4 m-4 h-8 w-8"></div>
+                </div>
+              ) : null}
+            </div>
+
+            <div className="text-sm">
+              <Link
+                to="/signup"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                New Here! Sign Up!
+              </Link>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                <span className="absolute font-bold inset-y-0 left-0 flex items-center pl-3"></span>
+                Sign In
+              </button>
+            </div>
+          </Form>
+        </Formik>
+        <ToastContainer
+        position="bottom-left"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      </div>
+    </div>
+  );
+}
